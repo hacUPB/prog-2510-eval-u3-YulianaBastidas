c674cc79feaf56448261409e01b7a0637b9c6e62
YulianaBastidas <140168389+YulianaBastidas@users.noreply.github.com>
2025-03-18 15:37:48 -0500

    .

--- Actividad 3 Bucles y Ciclos/ejercicios.py
+++ Actividad 3 Bucles y Ciclos/ejercicios.py
@@ -15,3 +15,31 @@ lista3 = [434, "Susie", 1088]
 print(lista3)
 lista3.append(4756)
 
+
+# Creamos una cadena de caracteres
+cadena1 = "Hola"
+
+# Asignamos la cadena a otra variable
+cadena2 = cadena1
+
+# Intentamos modificar la cadena2 (esto generará un error)
+try:
+    cadena2 += " Mundo"
+except TypeError as e:
+    print(f"Error: {e}")
+
+# Imprimimos ambas cadenas
+print("Cadena 1:", cadena1)
+print("Cadena 2:", cadena2)
+
+
+# lista con top 5 de canciones favoritas
+
+canciones_fav = ["Superman", "Cinnamon Girl", "Everybody Wants To Rule The World", "Unholy", "Back To Black"]
+cantantes_cfav = ["Eminem", "Lana Del Rey", "Tears For Fears", "Sam Smith", "Amy Winehouse"]
+
+indice = 0
+while indice < 5:
+    print(f"{canciones_fav[indice]} la interpreta {cantantes_cfav[indice]}")
+    indice += 1
+# Con este código inicializamos en 0 y vamos hasta 5, ya que tanto canciones como cantantes fav solo tenemos 4
